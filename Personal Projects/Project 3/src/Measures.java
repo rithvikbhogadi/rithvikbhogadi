c004bfec86eb221b05ca56b720b86cfaf3adcc99
rithvikbhogadi <98076087+rithvikbhogadi@users.noreply.github.com>
2023-10-13 17:27:54 -0400

    Delete Personal Projects/Project 3 directory

--- Personal Projects/Project 3/src/Measures.java
+++
@@ -1,7 +0,0 @@
-package src;
-
-public interface Measures
-{
-    public double[] measures(IndicatorT ind) throws UnsupportedOperationException;
-    public double[] measures(AttributeT att) throws UnsupportedOperationException;
-}
